7a14a92e0a256b2409f0dfc610969c1189556638
Jorrit Scholten <jorrit.scholten@hotmail.com>
2023-07-18 12:58:36 +0200

    added empty Spring Boot project as created by the initializer

---
+++ src/main/java/com/github/quizapp/OcaQuizAppApplication.java
@@ -0,0 +1,12 @@
+package com.github.quizapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcaQuizAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OcaQuizAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/quizapp/OcaQuizAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.quizapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcaQuizAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
