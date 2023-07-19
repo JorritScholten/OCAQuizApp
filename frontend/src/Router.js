6c9c898b5dca6af8f738ec63a82197afbe827573
Jorrit Scholten <jorrit.scholten@hotmail.com>
2023-07-19 11:40:36 +0200

    created simple yes/no question creation form

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
+import Question from "./pages/question";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/question" element={<Question />} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
     </BrowserRouter>
