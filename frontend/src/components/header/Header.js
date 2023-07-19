10fd8e81ad0e20a297927bb69b280908dac38baa
janne <ennaj_janne@hotmail.com>
2023-07-19 16:00:08 +0200

    try to make a home button for in the header

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import LoginButton from "../loginButton";
+import HomeButton from "../homeButton";
 
 export default function Header({ title = "Header" }) {
   return (
     <div className="top-0 z-50 w-full flex justify-center items-center h-24 bg-orange-400 text-gray-800 font-bold text-4xl shadow-md border border-gray-200">
-      {title}
+      <HomeButton /> {title} <LoginButton />
     </div>
   );
 }
