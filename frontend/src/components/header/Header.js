048f9806b9b9f2db044b6443aa96129d00e2d891
janne <ennaj_janne@hotmail.com>
2023-07-20 16:03:26 +0200

    comment for what to do next

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -2,6 +2,8 @@ import React from "react";
 import LoginButton from "../loginButton";
 import HomeButton from "../homeButton";
 
+// with javaScript ensure that Home can be clicked and go to the home page. the same goes for the login button.
+
 export default function Header() {
   return (
     <div className="flex justify-between items-center px-4 py-2 bg-green-500 text-gray-800 font-bold text-4xl shadow-md border border-gray-200">
