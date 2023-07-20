c8a54dd8f284023187de8b85ec9cefd5ba51ba4e
janne <ennaj_janne@hotmail.com>
2023-07-20 15:49:42 +0200

    Title placed in header.

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -6,7 +6,9 @@ export default function Header() {
   return (
     <div className="flex justify-between items-center px-4 py-2 bg-green-500 text-gray-800 font-bold text-4xl shadow-md border border-gray-200">
       <HomeButton />
-
+      <h1>
+        JAVA OCA 8 Quiz <h2>from Mala Gupta book</h2>
+      </h1>
       <LoginButton />
     </div>
   );
