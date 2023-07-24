5afff4cf063837c5245a94d47fbcbf600fdae7ce
janne <ennaj_janne@hotmail.com>
2023-07-24 10:52:49 +0200

    New folder and file for login page.

--- frontend/src/components/homeButton/HomeButton.js
+++ frontend/src/components/homeButton/HomeButton.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Home() {
   return (
-    <button className="bg-green-600 text-white px-2 py-2 rounded">Home</button>
+    <button onClick={}  className="bg-green-600 text-white px-2 py-2 rounded">Home</button>
   );
 }
