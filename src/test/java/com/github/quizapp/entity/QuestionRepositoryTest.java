3a40bc5ffe57737e3704378d49e9a0eef577d398
Jorrit Scholten <jorrit.scholten@hotmail.com>
2023-07-24 22:03:09 +0200

    Changed function parameters to List because generic is better
    Also removed a few occurrences of System.out.println() and random spaces in TextFormatEnforcement.java. Added two tests to textFormatEnforcementTest.java to get a feel for how to use the class.

--- src/test/java/com/github/quizapp/entity/QuestionRepositoryTest.java
+++ src/test/java/com/github/quizapp/entity/QuestionRepositoryTest.java
@@ -1,12 +1,4 @@
 package com.github.quizapp.entity;
 
-import org.junit.jupiter.api.Test;
-
 public class QuestionRepositoryTest {
-    @Test
-    void helloWorld(){
-        System.out.println("Hello world");
-        assert(false);
-        assert(true);
-    }
 }
