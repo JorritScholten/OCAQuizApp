71c7c44cc6872a1db980f3b35fca5ec5976328dc
janne <ennaj_janne@hotmail.com>
2023-07-24 14:48:12 +0200

    questionQuizButton added to Home.

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../components/header";
+import CreateQuizQuestionButton from "./components/createQuizQuestion/CreateQuizQuestionButton";
 
 export default function Home() {
   return (
     <div className="w-screen flex flex-col">
       <Header title="Homepage" />
       <p>home page</p>
+      <CreateQuizQuestionButton />
     </div>
   );
 }
