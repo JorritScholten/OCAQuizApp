6c567808586e073b5e685bfd70e96aef9caf2688
janne <ennaj_janne@hotmail.com>
2023-07-24 11:41:18 +0200

    Login button linkt to login page.

--- frontend/src/components/loginButton/LoginButton.js
+++ frontend/src/components/loginButton/LoginButton.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
-    <button className="bg-green-600 text-white px-2 py-2 rounded">Login</button>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link
+              to="/login"
+              className="bg-green-600 text-white px-2 py-2 rounded"
+            >
+              Login
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 }
