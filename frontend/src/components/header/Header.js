55b27dc40924026a23da1923055f0b3ba8d74a15
janne <ennaj_janne@hotmail.com>
2023-07-24 15:33:22 +0200

    Try to merge but there goes something wrong.

---
+++ frontend/src/components/header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import LoginButton from "../loginButton";
+import HomeButton from "../homeButton";
+
+// with javaScript ensure that Home can be clicked and go to the home page. the same goes for the login button.
+
+export default function Header() {
+  return (
+    <div className="flex justify-between items-center px-4 py-2 bg-green-500 text-gray-800 font-bold text-4xl shadow-md border border-gray-200">
+      <HomeButton />
+      <h1>
+        JAVA OCA 8 Quiz <h2>from Mala Gupta book</h2>
+      </h1>
+      <LoginButton />
+    </div>
+  );
+}
