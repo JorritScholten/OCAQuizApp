be2df931c52ac7db1efe7baf4c3a94beedbdd5fd
janne <ennaj_janne@hotmail.com>
2023-07-25 12:23:18 +0200

    Input form adapted yes no question.

--- frontend/src/pages/question/Question.jsx
+++ frontend/src/pages/question/Question.jsx
@@ -19,12 +19,62 @@ function CreateYesNo() {
     >
       <h2>Yes/No question</h2>
       <label htmlFor="title">
-        Title:
+        Question:
         <input
           type="text"
           id="title"
+          // The question must start with a capital letter and end with a full stop or question mark.
           pattern="[A-Z]{1}[ \S]+[.?]{1}"
           required
+        />
+      </label>
+      <button type="submit" id="type" value="yesno" className="bg-blue-300">
+        Submit
+      </button>
+      <label htmlFor="answer">
+        Answer:
+        <input
+          type="text"
+          id="answer"
+          // REGEX
+          pattern=""
+          required
+        />
+      </label>
+      <button type="submit" id="type" value="yesno" className="bg-blue-300">
+        Submit
+      </button>
+      <label htmlFor="name">
+        tag:
+        <input
+          type="text"
+          id="name"
+          // REGEX
+          pattern=""
+        />
+      </label>
+      <button type="submit" id="type" value="yesno" className="bg-blue-300">
+        Submit
+      </button>
+      <label htmlFor="name">
+        tag:
+        <input
+          type="text"
+          id="name"
+          // REGEX
+          pattern=""
+        />
+      </label>
+      <button type="submit" id="type" value="yesno" className="bg-blue-300">
+        Submit
+      </button>
+      <label htmlFor="name">
+        tag:
+        <input
+          type="text"
+          id="name"
+          // REGEX
+          pattern=""
         />
       </label>
       <button type="submit" id="type" value="yesno" className="bg-blue-300">
