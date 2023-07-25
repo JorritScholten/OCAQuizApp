f5b83da7c89a9a66b6e975c29cb2208a06183b5e
Jorrit Scholten <jorrit.scholten@hotmail.com>
2023-07-25 15:40:16 +0200

    tag creation works, TODO: update tag list when creating new

--- frontend/src/pages/tagManagement/TagManagement.jsx
+++ frontend/src/pages/tagManagement/TagManagement.jsx
@@ -7,7 +7,50 @@ export default function TagManagement() {
     <div className="w-screen flex flex-col">
       <Header />
       <h1>Tag management</h1>
+      <CreateTag />
       <ShowTags />
+    </div>
+  );
+}
+
+function CreateTag() {
+  const [tag, setTag] = useState("");
+  const submitTag = async (e) => {
+    e.preventDefault();
+    fetch("http://localhost:8080/api/v1/tag", {
+      method: "POST",
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+      },
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify({
+        name: tag,
+      }),
+    }).then((res) => {
+      console.log(res);
+      setTag("");
+      // UpdateTags();
+    });
+  };
+  return (
+    <div className="bg-slate-100 m-2">
+      <h2 className="text-center">Create new tag</h2>
+      <form className="flex flex-col" onSubmit={submitTag}>
+        <label htmlFor="name">
+          Name:
+          <input
+            type="text"
+            value={tag}
+            id="name"
+            onChange={(e) => setTag(e.target.value)}
+          />
+        </label>
+        <button className="items-center m-1 bg-green-300" type="submit">Create</button>
+      </form>
     </div>
   );
 }
@@ -23,7 +66,7 @@ function ShowTags() {
 
   return (
     <>
-      <div className="bg-slate-100 mx-2 pb-2">
+      <div className="bg-slate-100 m-2 pb-2">
         <div className="flex flex-row justify-around py-2 items-center">
           <div />
           <h2 className="">All tags</h2>
