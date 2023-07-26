7a69b88e150ab3fbf3b4e5778cbcbd7f75fc3633
Jorrit Scholten <jorrit.scholten@hotmail.com>
2023-07-26 21:54:10 +0200

    created question type selection

--- frontend/src/pages/question/Question.jsx
+++ frontend/src/pages/question/Question.jsx
@@ -1,35 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../components/header";
 export default function Question() {
+  const typeOptions = ["Yes/No", "Multiple choice", "Multiple selection"];
+  const [typeChoice, setTypeChoice] = useState(typeOptions[1]);
+
   return (
     <div className="w-screen flex flex-col">
       <Header title="Question" />
       <h1 className="self-center">Create a question</h1>
-      <CreateYesNo />
+      <div className="bg-slate-100 flex flex-col gap-2 m-2 p-2">
+        <div className="flex flex-row gap-2 content-evenly">
+          {typeOptions.map((typeOption) => {
+            if (typeChoice === typeOption) {
+              return (
+                <div
+                  className="bg-green-300 w-full place-self-center text-center"
+                  key={typeOption}
+                >
+                  {typeOption}
+                </div>
+              );
+            } else {
+              return (
+                <button
+                  className="bg-slate-300 w-full place-self-center text-center"
+                  key={typeOption}
+                  onClick={() => setTypeChoice(typeOption)}
+                >
+                  {typeOption}
+                </button>
+              );
+            }
+          })}
+        </div>
+        <div className="w-full px-2">
+          {typeChoice === typeOptions[0] ? (
+            <div>yes/no question undefined</div>
+          ) : null}
+          {typeChoice === typeOptions[1] ? (
+            <div>choice question undefined</div>
+          ) : null}
+          {typeChoice === typeOptions[2] ? (
+            <div>selection question undefined</div>
+          ) : null}
+        </div>
+      </div>
     </div>
-  );
-}
-
-function CreateYesNo() {
-  return (
-    <form
-      className="grid w-full md:justify-start justify-center bg-slate-200"
-      method="post"
-      encType="multipart/form-data"
-    >
-      <h2>Yes/No question</h2>
-      <label htmlFor="title">
-        Title:
-        <input
-          type="text"
-          id="title"
-          pattern="[A-Z]{1}[ \S]+[.?]{1}"
-          required
-        />
-      </label>
-      <button type="submit" id="type" value="yesno" className="bg-blue-300">
-        Submit
-      </button>
-    </form>
   );
 }
