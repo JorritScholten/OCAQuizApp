29eccfdf6fd1b8074751426ccb4605f65e602833
janne <ennaj_janne@hotmail.com>
2023-07-27 11:09:47 +0200

    Header text adapted to mobile and pc screen.

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import LoginButton from "../loginButton";
 import HomeButton from "../homeButton";
 
-// with javaScript ensure that Home can be clicked and go to the home page. the same goes for the login button.
-
 export default function Header() {
   return (
     <div className="flex justify-between items-center px-4 py-2 bg-green-500 text-gray-800 font-bold text-4xl shadow-md border border-gray-200">
       <HomeButton />
-      <h1>
-        JAVA OCA 8 Quiz <h2>from Mala Gupta book</h2>
+
+      <h1 className="text-center text-lg md:text-2xl place-self-center">
+        JAVA OCA 8 Quiz
+        <h2 className="text-sm md:text-xl">from Mala Gupta book</h2>
       </h1>
+      {/*Temporarily removed the login button because he has no function yet. */}
       <LoginButton />
     </div>
   );
