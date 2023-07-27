2973ced168cd3e89449198b3834d053b05123b47
janne <ennaj_janne@hotmail.com>
2023-07-27 11:43:33 +0200

    Home text removed by whitening it.

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="w-screen flex flex-col">
       <Header />
-      <h1>home page</h1>
+      <h1 className="text-white">home page</h1>
       <div className="flex flex-col gap-2 m-2">
         <GotoButton to="/question" text="Create a new question" />
         <GotoButton to="/tagManagement" text="Tag Management" />
