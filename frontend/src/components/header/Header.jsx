418723ef085e7ac21c71b191bbae8a60b07ebbdb
janne <ennaj_janne@hotmail.com>
2023-07-27 11:21:10 +0200

    Temporarily removed the login button because he has no function yet.

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import LoginButton from "../loginButton";
+// import LoginButton from "../loginButton";
 import HomeButton from "../homeButton";
 
 export default function Header() {
   return (
-    <div className="flex justify-between items-center px-4 py-2 bg-green-500 text-gray-800 font-bold text-4xl shadow-md border border-gray-200">
+    <div className="flex content-around justify-between items-center px-4 py-2 bg-green-500 text-gray-800 font-bold text-4xl shadow-md border border-gray-200">
       <HomeButton />
 
       <h1 className="text-center text-lg md:text-2xl place-self-center">
@@ -12,7 +12,8 @@ export default function Header() {
         <h2 className="text-sm md:text-xl">from Mala Gupta book</h2>
       </h1>
       {/*Temporarily removed the login button because he has no function yet. */}
-      <LoginButton />
+      {/* <LoginButton /> */}
+      <div />
     </div>
   );
 }
