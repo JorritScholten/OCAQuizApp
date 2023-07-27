ddcde4e37f97a42e68492ed49cb5ae9f334b8ac0
janne <ennaj_janne@hotmail.com>
2023-07-27 11:35:59 +0200

    Temporarily added div instead of the login button.

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -4,7 +4,7 @@ import HomeButton from "../homeButton";
 
 export default function Header() {
   return (
-    <div className="flex content-around justify-between items-center px-4 py-2 bg-green-500 text-gray-800 font-bold text-4xl shadow-md border border-gray-200">
+    <div className="flex content-around justify-between items-center px-2 py-2 bg-green-500 text-gray-800 font-bold text-4xl shadow-md border border-gray-200">
       <HomeButton />
 
       <h1 className="text-center text-lg md:text-2xl place-self-center">
@@ -13,7 +13,10 @@ export default function Header() {
       </h1>
       {/*Temporarily removed the login button because he has no function yet. */}
       {/* <LoginButton /> */}
-      <div />
+      {/* Temporarily added div instead of the login button.*/}
+      <div className="bg-green-500 text-green-500 px-2 py-2 rounded text-lg md:text-2xl">
+        Login
+      </div>
     </div>
   );
 }
